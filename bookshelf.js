1365a94078e12a746a813b9432b4340a240b8bfc
Rhys van der Waerden <rhys.vdw@gmail.com>
2015-06-14 19:29:35 +1000

    Experimenting with different jsDoc namepath options.

--- bookshelf.js
+++ bookshelf.js
@@ -1,21 +1,9 @@
 /**
- * Bluebird
- * @external Promise
- * @see {@link https://github.com/petkaantonov/bluebird/ Bluebird}
- */
-
-/**
- * Knex.js
- * @external Knex
- * @see {@link https://knexjs.org Knex}
- */
-
-/**
  * (c) 2014 Tim Griesser
  * For all details and documentation:
  * http://bookshelfjs.org
  *
- * @name Bookshelf.js
+ * @module Bookshelf
  * @version 0.8.1
  * @license MIT
  *
